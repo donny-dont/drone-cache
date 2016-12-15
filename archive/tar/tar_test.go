7118e156869d8293e438c8e78ae1954ecaaea769
Jacob McCann <jmccann.git@gmail.com>
2016-12-15 12:15:58 -0600

    Remove fixtures from cache testing (#8)
    * Cleanup fixtures after tar package testing
    
    * Create fixture directories more programatically
    
    * Remove fixtures from cache testing

--- archive/tar/tar_test.go
+++ archive/tar/tar_test.go
@@ -21,94 +21,103 @@ func TestTarArchive(t *testing.T) {
 	g := Goblin(t)
 	wd, _ := os.Getwd()
 
-	// Create necessary fixtures
-	createFixtures()
-
-	g.Describe("New", func() {
-		g.It("Should return tarArchive", func() {
-			ta := New()
-			g.Assert(ta != nil).IsTrue("failed to create tarArchive")
-		})
-	})
-
-	g.Describe("Pack", func() {
-		g.It("Should return no error", func() {
-			ta := New()
-			g.Assert(ta != nil).IsTrue("failed to create tarArchive")
-
-			os.Chdir("/tmp/fixtures/mounts")
-			err, werr := packIt(ta, validMount, "/tmp/fixtures/tarfiles/test.tar")
-			os.Chdir(wd)
-
-			if err != nil {
-				fmt.Printf("Received unexpected err: %s\n", err)
-			}
-			g.Assert(err == nil).IsTrue("Failed to read the stream")
-			if werr != nil {
-				fmt.Printf("Received unexpected werr: %s\n", werr)
-			}
-			g.Assert(werr == nil).IsTrue("Failed to pack")
+	g.Describe("tar package", func() {
+		g.Before(func() {
+			// Create necessary fixtures
+			createFixtures()
 		})
 
-		g.It("Should return error if mount does not exist", func() {
-			ta := New()
-			g.Assert(ta != nil).IsTrue("failed to create tarArchive")
-
-			err, werr := packIt(ta, invalidMount, "/tmp/fixtures/tarfiles/invalidMount.tar")
-
-			g.Assert(err == nil).IsTrue("Failed to read the stream")
-			g.Assert(werr != nil).IsTrue("Failed to properly stat 'mount'")
-			g.Assert(werr.Error()).Equal("stat mount1: no such file or directory")
-		})
-	})
-
-	g.Describe("Unpack", func() {
-		g.It("Should return no error", func() {
-			ta := New()
-			g.Assert(ta != nil).IsTrue("failed to create tarArchive")
-
-			err := unpackIt(ta, validFile)
-
-			if err != nil {
-				fmt.Printf("Received unexpected err: %s\n", err)
-			}
-			g.Assert(err == nil).IsTrue("Failed to unpack")
+		g.After(func() {
+			// Remove fixtures
+			cleanFixtures()
 		})
 
-		g.It("Should create files in correct strucutre", func() {
-			g.Assert(exists("/tmp/extracted/test.txt")).IsTrue("failed to create test.txt")
-			g.Assert(exists("/tmp/extracted/subdir")).IsTrue("failed to create subdir")
-			g.Assert(exists("/tmp/extracted/subdir/test2.txt")).IsTrue("failed to create subdir/test2.txt")
+		g.Describe("New", func() {
+			g.It("Should return tarArchive", func() {
+				ta := New()
+				g.Assert(ta != nil).IsTrue("failed to create tarArchive")
+			})
 		})
 
-		g.It("Should create files with correct content", func() {
-			var err error
-			var content []byte
-			for _, element := range mountFiles {
-				content, err = ioutil.ReadFile("/tmp/extracted/" + element.Path)
-				g.Assert(err == nil).IsTrue("failed to read" + element.Path)
-				g.Assert(string(content)).Equal(element.Content)
-			}
+		g.Describe("Pack", func() {
+			g.It("Should return no error", func() {
+				ta := New()
+				g.Assert(ta != nil).IsTrue("failed to create tarArchive")
+
+				os.Chdir("/tmp/fixtures/mounts")
+				err, werr := packIt(ta, validMount, "/tmp/fixtures/tarfiles/test.tar")
+				os.Chdir(wd)
+
+				if err != nil {
+					fmt.Printf("Received unexpected err: %s\n", err)
+				}
+				g.Assert(err == nil).IsTrue("Failed to read the stream")
+				if werr != nil {
+					fmt.Printf("Received unexpected werr: %s\n", werr)
+				}
+				g.Assert(werr == nil).IsTrue("Failed to pack")
+			})
+
+			g.It("Should return error if mount does not exist", func() {
+				ta := New()
+				g.Assert(ta != nil).IsTrue("failed to create tarArchive")
+
+				err, werr := packIt(ta, invalidMount, "/tmp/fixtures/tarfiles/invalidMount.tar")
+
+				g.Assert(err == nil).IsTrue("Failed to read the stream")
+				g.Assert(werr != nil).IsTrue("Failed to properly stat 'mount'")
+				g.Assert(werr.Error()).Equal("stat mount1: no such file or directory")
+			})
 		})
 
-		g.It("Should return error on invalid tarfile", func() {
-			ta := New()
-			g.Assert(ta != nil).IsTrue("failed to create tarArchive")
+		g.Describe("Unpack", func() {
+			g.It("Should return no error", func() {
+				ta := New()
+				g.Assert(ta != nil).IsTrue("failed to create tarArchive")
 
-			err := unpackIt(ta, invalidFile)
+				err := unpackIt(ta, validFile)
 
-			g.Assert(err != nil).IsTrue("Failed to return error")
-			g.Assert(err.Error()).Equal("unexpected EOF")
-		})
+				if err != nil {
+					fmt.Printf("Received unexpected err: %s\n", err)
+				}
+				g.Assert(err == nil).IsTrue("Failed to unpack")
+			})
 
-		g.It("Should return error on missing file", func() {
-			ta := New()
-			g.Assert(ta != nil).IsTrue("failed to create tarArchive")
+			g.It("Should create files in correct strucutre", func() {
+				g.Assert(exists("/tmp/extracted/test.txt")).IsTrue("failed to create test.txt")
+				g.Assert(exists("/tmp/extracted/subdir")).IsTrue("failed to create subdir")
+				g.Assert(exists("/tmp/extracted/subdir/test2.txt")).IsTrue("failed to create subdir/test2.txt")
+			})
 
-			err := unpackIt(ta, missingFile)
+			g.It("Should create files with correct content", func() {
+				var err error
+				var content []byte
+				for _, element := range mountFiles {
+					content, err = ioutil.ReadFile("/tmp/extracted/" + element.Path)
+					g.Assert(err == nil).IsTrue("failed to read" + element.Path)
+					g.Assert(string(content)).Equal(element.Content)
+				}
+			})
 
-			g.Assert(err != nil).IsTrue("Failed to return error")
-			g.Assert(err.Error()).Equal("open /tmp/fixtures/tarfiles/test2.tar: no such file or directory")
+			g.It("Should return error on invalid tarfile", func() {
+				ta := New()
+				g.Assert(ta != nil).IsTrue("failed to create tarArchive")
+
+				err := unpackIt(ta, invalidFile)
+
+				g.Assert(err != nil).IsTrue("Failed to return error")
+				g.Assert(err.Error()).Equal("unexpected EOF")
+			})
+
+			g.It("Should return error on missing file", func() {
+				ta := New()
+				g.Assert(ta != nil).IsTrue("failed to create tarArchive")
+
+				err := unpackIt(ta, missingFile)
+
+				g.Assert(err != nil).IsTrue("Failed to return error")
+				g.Assert(err.Error()).Equal("open /tmp/fixtures/tarfiles/test2.tar: no such file or directory")
+			})
 		})
 	})
 }
@@ -179,19 +188,28 @@ func createMountContent() {
 }
 
 func createFixtures() {
-	// Create directories if does not exist
-	if _, err := os.Stat("/tmp/fixtures/tarfiles"); os.IsNotExist(err) {
-		os.MkdirAll("/tmp/fixtures/tarfiles", os.FileMode(int(0755)))
-	}
-	if _, err := os.Stat("/tmp/fixtures/mounts/subdir"); os.IsNotExist(err) {
-		os.MkdirAll("/tmp/fixtures/mounts/subdir", os.FileMode(int(0755)))
-	}
-	if _, err := os.Stat("/tmp/extracted"); os.IsNotExist(err) {
-		os.MkdirAll("/tmp/extracted", os.FileMode(int(0755)))
-	}
-
+	createDirectories()
 	createBadTarfile()
 	createMountContent()
+}
+
+func cleanFixtures() {
+	os.RemoveAll("/tmp/fixtures/")
+	os.RemoveAll("/tmp/extracted/")
+}
+
+func createDirectories() {
+	directories := []string{
+		"/tmp/fixtures/tarfiles",
+		"/tmp/fixtures/mounts/subdir",
+		"/tmp/extracted",
+	}
+
+	for _, directory := range directories {
+		if _, err := os.Stat(directory); os.IsNotExist(err) {
+			os.MkdirAll(directory, os.FileMode(int(0755)))
+		}
+	}
 }
 
 func exists(path string) bool {
